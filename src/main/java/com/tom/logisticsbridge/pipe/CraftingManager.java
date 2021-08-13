a767c3d4dbe66ea21fa3f83355b82fbd364146c8
Li <nhatlinh.l195@gmail.com>
2021-08-13 20:03:12 +0700

    Update CraftingManager.java

--- src/main/java/com/tom/logisticsbridge/pipe/CraftingManager.java
+++ src/main/java/com/tom/logisticsbridge/pipe/CraftingManager.java
@@ -459,6 +459,8 @@ public class CraftingManager extends PipeLogisticsChassis implements IIdPipe {
                                     IRouter router = SimpleServiceLocator.routerManager.getRouter(SimpleServiceLocator.routerManager.getIDforUUID(en.getKey()));
                                     if (router != null)
                                         maxDist = Math.max(maxDist, (int) router.getPipe().getPos().distanceSq(getPos()));
+				    else
+					maxDist = Math.max(maxDist, (int) en.getKey().getRouter().getPipe().getPos().distanceSq(getPos()));
                                 }
                             }
                             useEnergy(neededEnergy(), true);
